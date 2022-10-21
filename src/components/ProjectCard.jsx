c3281869177578d028eed7949d3aabdc70de7bc3
Simone Teglia <simone.teglia9@gmail.com>
2022-10-22 00:08:16 +0200

    Ascoltale link working

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,17 +1,25 @@
 import React, { useState } from "react";
+import "../resources/styles.css";
 
 export default function ProjectCard({ title, bgColor }) {
 	return (
 		<div
-			className="project-card"
+			className="project-card text"
 			style={{
 				flex: "1 1 500px",
 				height: "80%",
 				backgroundColor: bgColor,
+				cursor: "pointer",
 			}}
-			onMouseEnter={() => {
+			// onMouseEnter={() => {
+			// 	switch (title) {
+			// 		case "css art":
+			// 	}
+			// }}
+			onClick={() => {
 				switch (title) {
-					case "css art":
+					case "ascoltale":
+						window.location.href = "https://ascoltale.vercel.app";
 				}
 			}}
 		>
