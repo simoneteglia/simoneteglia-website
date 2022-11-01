549e6239235d1022c98b1fe5066a6061e5221258
Simone Teglia <simone.teglia9@gmail.com>
2022-11-01 09:16:40 +0100

    Pixelator page continued

---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft, faArrowDown } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+const akiraFont = "Akira";
+
+export default function Header({ title }) {
+	return (
+		<div
+			style={{
+				width: "100%",
+				height: "30vh",
+				display: "flex",
+				alignItems: "center",
+				justifyContent: "center",
+				fontSize: "60px",
+				fontFamily: akiraFont,
+				color: "yellow",
+				position: "relative",
+			}}
+		>
+			<h1>{title}</h1>
+			<Link
+				to="/"
+				style={{ position: "absolute", left: "3vw", color: "yellow" }}
+			>
+				<FontAwesomeIcon icon={faArrowLeft} />
+			</Link>
+		</div>
+	);
+}
--- src/components/PixelatorPage.jsx
+++ src/components/PixelatorPage.jsx
@@ -1,4 +1,9 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Header from "./Header";
+import "../resources/styles.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
 
 export default function PixelatorPage({}) {
 	useEffect(() => {
@@ -7,8 +12,33 @@ export default function PixelatorPage({}) {
 	}, []);
 
 	return (
-		<div id="pixelator-page" style={{ height: "100vh" }}>
-			<h1>Pixelator</h1>
+		<div
+			id="pixelator-page"
+			style={{
+				height: "100vh",
+				display: "flex",
+				flexDirection: "column",
+				alignItems: "center",
+				padding: "0 3vw",
+			}}
+		>
+			<Header title={"pixelator"} />
+			<div className="description">
+				<p>
+					Pixelator is a little python programm that takes a photo as
+					input and returns a pixelated version of it. I created this
+					program when trying to create some fancy{" "}
+					<Link
+						to="/css-art"
+						style={{
+							textDecoration: "underline",
+							color: "deeppink",
+						}}
+					>
+						css pixel art
+					</Link>
+				</p>
+			</div>
 		</div>
 	);
 }
